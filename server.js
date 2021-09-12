da22ca46ad0a23dd2a45190219d2adec853603d5
Vitor Oliveira <vbrazo@gmail.com>
2021-09-12 10:27:22 -0700

    Add listRoutes()

--- server.js
+++ server.js
@@ -34,7 +34,35 @@ app.get('/', (req, res) => {
 // Require Notes routes
 require('./app/routes/note.routes.js')(app);
 
+function listRoutes(routes, stack, parent){
+  parent = parent || '';
+  if(stack){
+    stack.forEach(function(r){
+      if (r.route && r.route.path){
+        var method = '';
+
+        for(method in r.route.methods){
+          if(r.route.methods[method]){
+            routes.push({method: method.toUpperCase(), path: parent + r.route.path});
+          }
+        }
+
+      } else if (r.handle && r.handle.name == 'router') {
+        const routerName = r.regexp.source.replace("^\\","").replace("\\/?(?=\\/|$)","");
+        return listRoutes(routes, r.handle.stack, parent + routerName);
+      }
+    });
+    return routes;
+  } else {
+    return listRoutes([], app._router.stack);
+  }
+}
+
+const routes = listRoutes();
+
 // listen for requests
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+const server = app.listen(80, function(){
+    console.log("Server is listening on port 80");
 });
+
+module.exports = server;
