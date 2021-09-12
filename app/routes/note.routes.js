35383ecad589c4765a4c712c72450258fde2cc76
Vitor Oliveira <vbrazo@gmail.com>
2021-09-12 08:37:02 -0700

    Add create controller and route

---
+++ app/routes/note.routes.js
@@ -0,0 +1,6 @@
+module.exports = (app) => {
+    const notes = require('../controllers/note.controller.js');
+
+    // Create a new Note
+    app.post('/notes', notes.create);
+}
--- server.js
+++ server.js
@@ -31,6 +31,9 @@ app.get('/', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
 
+// Require Notes routes
+require('./app/routes/note.routes.js')(app);
+
 // listen for requests
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
