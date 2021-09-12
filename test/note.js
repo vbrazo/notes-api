68bfaaad327920bf0351d0dcf5ad7a7a28db6f72
Vitor Oliveira <vbrazo@gmail.com>
2021-09-12 12:26:56 -0700

    Fix lint

--- test/note.js
+++ test/note.js
@@ -13,23 +13,23 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 describe('Notes', () => {
-    beforeEach((done) => {
-        Note.remove({}, (err) => {
-           done();
-        });
+  beforeEach((done) => {
+    Note.remove({}, (err) => {
+     done();
     });
+  });
 
   describe('/GET notes', () => {
-      it('it should GET all the notes', (done) => {
-        chai.request(server)
-            .get('/notes')
-            .end((err, res) => {
-                  res.should.have.status(200);
-                  res.body.should.be.a('array');
-                  res.body.length.should.be.eql(0);
-              done();
-            });
-      });
+    it('it should GET all the notes', (done) => {
+    chai.request(server)
+        .get('/notes')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          res.body.length.should.be.eql(0);
+          done();
+        });
+    });
   });
 
   describe('/POST notes', () => {
@@ -51,20 +51,19 @@ describe('Notes', () => {
 
   describe('/GET/:id notes', () => {
     it('it should GET a note by the given id', (done) => {
-        let note = new Note({ title: "The Lord of the Rings", content: "J.R.R. Tolkien" });
-        note.save((err, note) => {
-            chai.request(server)
-          .get('/notes/' + note.id)
-          .send(note)
-          .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.have.property('title');
-                res.body.should.have.property('_id').eql(note.id);
-            done();
-          });
+      let note = new Note({ title: "The Lord of the Rings", content: "J.R.R. Tolkien" });
+      note.save((err, note) => {
+          chai.request(server)
+        .get('/notes/' + note.id)
+        .send(note)
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('title');
+            res.body.should.have.property('_id').eql(note.id);
+          done();
         });
-
+      });
     });
   });
 
@@ -73,13 +72,13 @@ describe('Notes', () => {
          let note = new Note({title: "The Chronicles of Narnia", content: "C.S. Lewis"})
          note.save((err, note) => {
                chai.request(server)
-               .put('/notes/' + note.id)
-               .send({title: "The Chronicles of Narnia", content: "C.S."})
-               .end((err, res) => {
-                     res.should.have.status(200);
-                     res.body.should.be.a('object');
-                 done();
-               });
+                   .put('/notes/' + note.id)
+                   .send({title: "The Chronicles of Narnia", content: "C.S."})
+                   .end((err, res) => {
+                         res.should.have.status(200);
+                         res.body.should.be.a('object');
+                     done();
+                   });
          });
      });
   });
@@ -88,7 +87,7 @@ describe('Notes', () => {
     it('it should DELETE a note given the id', (done) => {
         let note = new Note({title: "The Chronicles of Narnia", content: "C.S. Lewis"})
         note.save((err, note) => {
-              chai.request(server)
+          chai.request(server)
               .delete('/notes/' + note.id)
               .end((err, res) => {
                     res.should.have.status(200);
