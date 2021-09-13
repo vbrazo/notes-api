83d15b0784490ba4d1f81e3c18b54feec716de6f
Vitor Oliveira <vbrazo@gmail.com>
2021-09-12 21:14:19 -0700

    add scripts npm lint

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 // create express app
 const app = express();
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Configuring the database
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
+const dbConfig = require("./config/database.config.js");
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
@@ -22,25 +22,25 @@ mongoose.connect(dbConfig.url, {
 }).then(() => {
     console.log("Successfully connected to the database");
 }).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
+    console.log("Could not connect to the database. Exiting now...", err);
     process.exit();
 });
 
 // define a simple route
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.json({"message": "Welcome to Notes API. Take notes quickly. Organize and keep track of all your notes."});
 });
 
 // Require Notes routes
-require('./app/routes/note.routes.js')(app);
-require('./app/routes/user.routes.js')(app);
+require("./app/routes/note.routes.js")(app);
+require("./app/routes/user.routes.js")(app);
 
 function listRoutes(routes, stack, parent){
-  parent = parent || '';
+  parent = parent || "";
   if(stack){
     stack.forEach(function(r){
       if (r.route && r.route.path){
-        var method = '';
+        var method = "";
 
         for(method in r.route.methods){
           if(r.route.methods[method]){
@@ -48,7 +48,7 @@ function listRoutes(routes, stack, parent){
           }
         }
 
-      } else if (r.handle && r.handle.name == 'router') {
+      } else if (r.handle && r.handle.name == "router") {
         const routerName = r.regexp.source.replace("^\\","").replace("\\/?(?=\\/|$)","");
         return listRoutes(routes, r.handle.stack, parent + routerName);
       }
--- test/note.js
+++ test/note.js
@@ -1,107 +1,121 @@
 //During the test the env variable is set to test
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
-let mongoose = require("mongoose");
-let Note = require('../app/models/note.model.js');
+let Note = require("../app/models/note.model.js");
 
 //Require the dev-dependencies
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../server');
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../server");
 let should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Notes', () => {
-    beforeEach((done) => {
-        Note.remove({}, (err) => {
-           done();
-        });
-    });
+describe("Notes", () => {
+  beforeEach((done) => {
+      Note.remove({}, (err) => {
+         done();
+      });
+  });
 
-  describe('/GET notes', () => {
-      it('it should GET all the notes', (done) => {
+  describe("/GET notes", () => {
+      it("it should GET all the notes", (done) => {
         chai.request(server)
-            .get('/notes')
+            .get("/notes")
             .end((err, res) => {
                   res.should.have.status(200);
-                  res.body.should.be.a('array');
+                  res.body.should.be.a("array");
                   res.body.length.should.be.eql(0);
               done();
             });
       });
   });
 
-  describe('/POST notes', () => {
-    it('it should POST a note', (done) => {
-        let note = {
-            title: "The Lord of the Rings",
-            content: "J.R.R. Tolkien"
-        }
+  describe("/POST notes", () => {
+    it("it should POST a note", (done) => {
+      let note = {
+          title: "The Lord of the Rings",
+          content: "J.R.R. Tolkien"
+      };
+
       chai.request(server)
-          .post('/notes')
+          .post("/notes")
           .send(note)
           .end((err, res) => {
                 res.should.have.status(200);
-                res.body.should.be.a('object');
-                should.equal(res.body['title'], note['title']);
-                should.equal(res.body['content'], note['content']);
+                res.body.should.be.a("object");
+                should.equal(res.body["title"], note["title"]);
+                should.equal(res.body["content"], note["content"]);
             done();
           });
     });
   });
 
+  describe("/GET/:id note", () => {
+    it("it should GET a note by the given id", (done) => {
+        let note = new Note({
+          title: "The Lord of the Rings",
+          content: "J.R.R. Tolkien"
+        });
 
-    describe('/GET/:id note', () => {
-      it('it should GET a note by the given id', (done) => {
-          let note = new Note({ title: "The Lord of the Rings", content: "J.R.R. Tolkien" });
-          note.save((err, note) => {
+        note.save((err, note) => {
+            chai.request(server)
+          .get("/notes/" + note.id)
+          .send(note)
+          .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a("object");
+                res.body.should.have.property("title");
+                res.body.should.have.property("_id").eql(note.id);
+                should.equal(res.body["title"], note.title);
+                should.equal(res.body["content"], note.content);
+            done();
+          });
+        });
+
+    });
+  });
+
+  describe("/PUT/:id note", () => {
+     it("it should UPDATE a note given the id", (done) => {
+         let note = new Note({
+           title: "The Chronicles of Narnia",
+           content: "C.S. Lewis"
+         });
+
+         note.save((err, note) => {
+               chai.request(server)
+               .put("/notes/" + note.id)
+               .send({
+                 title: "The Chronicles of Narnia",
+                 content: "C.S."
+               })
+               .end((err, res) => {
+                     res.should.have.status(200);
+                     res.body.should.be.a("object");
+                     should.equal(res.body["title"], "The Chronicles of Narnia");
+                     should.equal(res.body["content"], "C.S.");
+                 done();
+               });
+         });
+     });
+  });
+
+  describe("/DELETE/:id note", () => {
+    it("it should DELETE a note given the id", (done) => {
+        let note = new Note({
+          title: "The Chronicles of Narnia",
+          content: "C.S. Lewis"
+        });
+
+        note.save((err, note) => {
               chai.request(server)
-            .get('/notes/' + note.id)
-            .send(note)
-            .end((err, res) => {
-                  res.should.have.status(200);
-                  res.body.should.be.a('object');
-                  res.body.should.have.property('title');
-                  res.body.should.have.property('_id').eql(note.id);
-                  should.equal(res.body['title'], note.title);
-                  should.equal(res.body['content'], note.content);
-              done();
-            });
-          });
-
-      });
+                  .delete("/notes/" + note.id)
+                  .end((err, res) => {
+                        res.should.have.status(200);
+                    done();
+                  });
+        });
     });
-
-    describe('/PUT/:id note', () => {
-       it('it should UPDATE a note given the id', (done) => {
-           let note = new Note({title: "The Chronicles of Narnia", content: "C.S. Lewis"})
-           note.save((err, note) => {
-                 chai.request(server)
-                 .put('/notes/' + note.id)
-                 .send({title: "The Chronicles of Narnia", content: "C.S."})
-                 .end((err, res) => {
-                       res.should.have.status(200);
-                       res.body.should.be.a('object');
-                       should.equal(res.body['title'], "The Chronicles of Narnia");
-                       should.equal(res.body['content'], "C.S.");
-                   done();
-                 });
-           });
-       });
-    });
-
-    describe('/DELETE/:id note', () => {
-      it('it should DELETE a note given the id', (done) => {
-          let note = new Note({title: "The Chronicles of Narnia", content: "C.S. Lewis"})
-          note.save((err, note) => {
-                chai.request(server)
-                .delete('/notes/' + note.id)
-                .end((err, res) => {
-                      res.should.have.status(200);
-                  done();
-                });
-          });
-      });
-    });
+  });
 });
